fcb7f5a4ec811b1ebab7ba0ff7e477029a3ca8bc
melongood <51852601+melongood@users.noreply.github.com>
2019-06-22 17:31:05 +0900

    Update melon.py

--- melon.py
+++ melon.py
@@ -9,28 +9,16 @@ client = discord.Client()
 async def on_ready():
     print(client.user.id)
     print("ready")
-    game = discord.Game("멜룬")
+    game = discord.Game("커뮤니티/사장님월급주세요)")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 
 
 @client.event
-async def on_message(message, choicenumber=None):
+async def on_message(message):
     global author
-    if message.content.startswith("!안녕"):
-        await message.channel.send("안녕하세요")
-
     if message.content.startswith("!도움말"):
-        await message.channel.send("아직은테스트중이라 명령어가별로 없습니다 1.!안녕리고하면 봇이 인사를해줍니다 2.!멜룬은?,!산삼은?,!하넬은?등등하면은 재밌는답을해줍니다 ")
-
-    if message.content.startswith("!멜룬은?"):
-        await message.channel.send("존맛탱")
-
-    if message.content.startswith("!산삼은?"):
-        await message.channel.send("영양분")
-
-    if message.content.startswith("!하넬은?"):
-        await message.channel.send("하넬....어어어어")
+        await message.channel.send("아직은베타라 명령어가없습니다 삐삑")
 
     if message.content.startswith("!채널메시지"):
         channel = message.content[7:25]
@@ -42,4 +30,9 @@ async def on_message(message, choicenumber=None):
         role = discord.utils.get(message.guild.roles, name="뮤트")
         await  author.add_roles(role)
 
-client.run("NTg4NjYzMjE3MDMxNDc5MzE2.XQRXig.DKzsr3EoMEzf9ZuV_zMlFPFXf8o")
+    if message.content.startswith("/dm"):
+        author = message.guild.get_member(int(message.content[4:22]))
+        msg = message.content[23:]
+        await author.send(msg)
+
+client.run("NTg5NjAyMjQzMjA0ODc0MjQw.XQWD9w.FeUj6T7nBK4k8dhuXyTxvNYyGuQ")
